ae88e4dda9cefea02380ef79a02263e9ec3374f2
Maximon9 <aamaximon989@gmail.com>
2025-03-14 17:47:56 -0500

    Updated: build.bat;

--- build.bat
+++ build.bat
@@ -5,11 +5,8 @@ setlocal
 set "venv_path=./.venv"
 
 if exist "%venv_path%" (
-    echo Virtual Enviornment exist: %venv_path%
     for /f "delims=" %%V in ('where python') do (
-        echo Checking: %%V
         if /I not "%%V"=="%venv_path%\Scripts\python.exe" (
-            echo Using: %%V
             "%%V" build.py
             goto :end
         )
@@ -21,4 +18,5 @@ if exist "%venv_path%" (
 
 :end
 endlocal
+: exit
 pause
